ec041245a4a3f581ae1891e3c3d1d4cea3632fa7
JaeHoon Lee <ljhgood9925@gmail.com>
2021-01-01 03:33:23 +0900

    메모 클릭 시 index로 가기

--- ToDoList/my_to_do_app/views.py
+++ ToDoList/my_to_do_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from .models import *
 
 # Create your views here.
@@ -12,5 +13,6 @@ def createTodo(request):
     user_input_str = request.POST["todoContent"]
     new_todo = Todo(content = user_input_str)
     new_todo.save()
+    return HttpResponseRedirect(reverse("index"))
     # return render(request, "my_to_do_app/index.html")
-    return HttpResponse("my_to_do_app first page" + user_input_str)
+    # return HttpResponse("my_to_do_app first page" + user_input_str)
